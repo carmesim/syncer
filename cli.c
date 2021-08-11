c78f4026856a7c9ef8d8960ccf9459bb34806d8d
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-08-11 00:41:45 -0300

    Fix CLI arg parsing

--- cli.c
+++ cli.c
@@ -33,13 +33,16 @@ cli_opts_t parse_opts(const int argc, char ** argv) {
     if(!dest_folder_exists) {
         if (mkdir(argv[2], 0777) != 0) {
             fprintf(stderr, "error: failed to create destination folder ('%s')\n", argv[2]);
+            _exit(1);
         }
         printf("warn: destination folder '%s' created.\n", argv[2]);
     }
 
     cli_opts_t opts;
-    opts.destination_path = argv[1];
-    opts.origin_path = argv[2];
+
+    // No need to strdup here since argvs survive throughout the entire main function
+    opts.origin_path = argv[1];
+    opts.destination_path = argv[2];
 
     return opts;
 }
--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 #include <unistd.h> // For getopt
 #include "cli.h"
 
+
 int main(int argc, char ** argv)
 {
     cli_opts_t opts = parse_opts(argc, argv);
-    printf("Hello World!\n");
+
+    printf("%s: backing up '%s' into '%s'.\n", argv[0], opts.origin_path, opts.destination_path);
+
     return 0;
 }
