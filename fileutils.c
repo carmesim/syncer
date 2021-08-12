6595fb2e2d9b0fe4da09598b03edded2afc8cc27
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-08-12 17:39:30 -0300

    Use perror in get_mod_time

--- fileutils.c
+++ fileutils.c
@@ -24,10 +24,12 @@ bool is_directory(const char * const path ){
     return S_ISDIR(path_stat.st_mode);
 }
 
+// Gets the file's last modified time.
+// Assumes that the file in the given path exists
 time_t get_mod_time(const char * const path){
     struct stat path_stat;
     if(stat(path, &path_stat) != 0) {
-        printf("Error while analyzing %s\n", path);
+        perror(path);
         return -1;
     }
     return path_stat.st_mtime;
