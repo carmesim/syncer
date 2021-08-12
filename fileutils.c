5c28e5e2b94a7df74cb205b111bef55e8d623752
Frederico J. R. Pelogia <fredpelogia@outlook.com>
2021-08-12 16:25:36 -0300

    Implement `get_mod_time` function

--- fileutils.c
+++ fileutils.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <sys/stat.h>
 #include <stdbool.h>
 #include <unistd.h>
@@ -21,4 +22,13 @@ bool is_directory(const char * const path ){
         return false;
     }
     return S_ISDIR(path_stat.st_mode);
+}
+
+time_t get_mod_time(const char * const path){
+    struct stat path_stat;
+    if(stat(path, &path_stat) != 0) {
+        printf("Error while analyzing %s\n", path);
+        return -1;
+    }
+    return path_stat.st_mtime;
 }
--- fileutils.h
+++ fileutils.h
@@ -5,5 +5,6 @@
 
 bool is_directory(const char * const path);
 bool file_exists(const char * const path);
+time_t get_mod_time(const char * const path);
 
 #endif // FILEUTILS_H
